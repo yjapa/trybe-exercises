ce06bcd3315e09b34d2890628020efd4fd6446e4
Japa <wesleyy_@live.com>
2021-05-25 13:14:19 -0300

    Realização dos exercícios 6 e 7.

--- Fundamentos do Desenvolvimento Web/Bloco 4 - Introdução à JavaScript e Lógica de Programação/dia4_Objetos e Funções/Exercícios/scripptsParteII.js
+++ Fundamentos do Desenvolvimento Web/Bloco 4 - Introdução à JavaScript e Lógica de Programação/dia4_Objetos e Funções/Exercícios/scripptsParteII.js
@@ -44,13 +44,62 @@
 
 // console.log(maiorPalavra(['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana'])); //Fernanda  ------------- Exercício 4
 
-function maisRepete (numeros) {
-  let numeroRepeticao = numeros[0];
-  for (let indice in numeros) {
-    if ( numero[indice] == numeroRepeticao) {
-      numeroRepeticao = numero[indice];
+// function maisRepete (numeros) {
+//   let numeroRepeticao = numeros[0];
+//   for (let indice in numeros) {
+//     if ( numero[indice] == numeroRepeticao) {
+//       numeroRepeticao = numero[indice];
+//     }
+//     return numeroRepeticao;
+//   }
+// }
+//   console.log(maisRepete[2, 3, 2, 5, 8, 2 ]);
+
+//   function maisRepetido(numeros) {
+//     let contRepetido = 0;
+//     let contNumero = 0;
+//     let indexNumeroRepetido = 0;
+//     for (let index in numeros) {
+//       let verificaNumero = numeros[index];
+//       for (let index2 in numeros) {
+//         if (verificaNumero === numeros[index2]) {
+//           contNumero += 1;
+//         }
+//       }
+//       if (contNumero > contRepetido) {
+//         contRepetido = contNumero;
+//         indexNumeroRepetido = index;
+//       }
+//       contNumero = 0;
+//     }
+//     return numeros[indexNumeroRepetido];
+//   }------------------------------------------------------Exercício 5
+
+// function somaNumero (numero) {
+//   let soma = 0;
+//   for ( let index = 0; index <= numero; index +=1) {
+//     soma = soma + index;
+//   }
+//   return soma;
+// }
+
+// console.log(somaNumero(5));
+
+function verificaFimPalavra(palavra, fimPalavra) {
+  let inversoPalavra = palavra.split('').reverse().join('');
+  let inversoFimPalavra = fimPalavra.split('').reverse().join('');
+  let result;
+
+  for (let index = 0; index < inversoFimPalavra.length; index += 1) {
+    if (inversoPalavra[index] !== inversoFimPalavra[index]) {
+      result = false;
+      break;
+    } else {
+      result = true;
     }
-    return numeroRepeticao;
   }
+  
+  return result;
 }
-  console.log(maisRepete[2, 3, 2, 5, 8, 2 ]);
+
+console.log(verificaFimPalavra('trybe', 'be'));
