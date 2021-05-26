8da9a512efe8a2a3db0e1d1a63b71f778251aabb
Japa <wesleyy_@live.com>
2021-05-26 20:43:12 -0300

    Adicionado exercicios parte I e II

---
+++ Fundamentos do Desenvolvimento Web/Bloco 5 -JavaScript: DOM, Eventos e Web Storage/dia1_DOM e Seletores/script.js
@@ -0,0 +1,8 @@
+document.getElementById("header-container").style.background = "green";
+document.getElementsByTagName("h3")[0].style.background = "purple";
+document.getElementsByTagName("h3")[1].style.background = "purple";
+document.getElementsByTagName("h3")[2].style.background = "black";
+document.getElementsByTagName("h3")[3].style.background = "black";
+document.getElementsByTagName("section")[0].style.background = "orange";
+document.getElementsByTagName("section")[1].style.background = "yellow";
+document.getElementById("footer-container").style.background = "green";
