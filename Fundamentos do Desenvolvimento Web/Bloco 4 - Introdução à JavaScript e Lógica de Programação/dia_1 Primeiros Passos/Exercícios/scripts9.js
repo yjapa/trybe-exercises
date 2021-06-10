6320b4ada2bad70c65b7d40aec01607ec35185cb
Japa <wesleyy_@live.com>
2021-06-10 19:03:07 -0300

    Atualizando exercicios

---
+++ Fundamentos do Desenvolvimento Web/Bloco 4 - Introdução à JavaScript e Lógica de Programação/dia_1 Primeiros Passos/Exercícios/scripts9.js
@@ -0,0 +1,9 @@
+let numero1 = 12;
+let numero2 = 148;
+let numero3 = 150;
+
+if ((numero1%2 === 1 || numero2%2 === 1 || numero3%2 === 1 )){
+  console.log("true");
+} else {
+  console.log("false");
+}
---
+++ Fundamentos do Desenvolvimento Web/Bloco 5 -JavaScript: DOM, Eventos e Web Storage/dia_3 Eventos/main.js
@@ -0,0 +1,51 @@
+const divUm = document.getElementById('divUm');
+const divDois = document.getElementById('divDois');
+const divTres = document.getElementById('divTres');
+const input = document.getElementById('input');
+const myWebpage = document.getElementById('mySpotrybefy');
+
+function recebeClick(eventoDeOrigem) {
+  let techButton = document.querySelector('.tech');
+  techButton.classList.remove('tech');
+  eventoDeOrigem.target.classList.add('tech');
+}
+
+
+divUm.addEventListener('click', recebeClick);
+divDois.addEventListener('click', recebeClick);
+divTres.addEventListener('click', recebeClick);
+
+function caixaTexto (evento){
+let techButton = document.querySelector('.tech');
+techButton.innerText = evento.target.value;
+}
+
+input.addEventListener('input', caixaTexto);
+
+let redirecionar = document.querySelector('#mySpotrybefy');
+
+redirecionar.addEventListener('dblclick', function (){
+  window.open('https://blog.betrybe.com/');
+})
+
+// podemos utilizar window.replace ou window.assing ( a diferença é que não abrem em outra aba e o replace não volta no browser)
+
+redirecionar.addEventListener('mouseover', function (e){
+  e.target.style.color = 'red';
+})
+
+redirecionar.addEventListener('mouseout', function (e){
+  e.target.style.color = 'white';
+})
+
+function resetText(event) {
+  // O Event é passado como um parâmetro para a função.
+  event.target.innerText = 'Opção reiniciada';
+  // O event possui várias propriedades, porém a mais usada é o event.target,
+  // que retorna o objeto que disparou o evento.
+}
+
+divUm.addEventListener('dblclick', resetText);
+// Não precisa passar o parâmetro dentro do addEventListener. O próprio
+// navegador fará esse trabalho por você, não é legal? Desse jeito, o
+// event.target na nossa função retornará o objeto 'divUm'.
