92e04f42758d6351bf0c52e710d981776f05ca47
Japa <wesleyy_@live.com>
2021-06-28 19:44:26 -0300

    Exercícios

---
+++ Fundamentos do Desenvolvimento Web/Bloco 8 - Higher Order Functions do JavaScript ES6/dia_4 reduce/script.js
@@ -0,0 +1,71 @@
+const collection = [1, 2, 3, 4, 5];
+
+const getSum = (accumulator, number) => {
+  console.log(accumulator); // 1 3 6 10
+  return accumulator + number;
+};
+
+const sumNumbers = collection.reduce(getSum);
+console.log(sumNumbers); // 15
+
+//
+
+const numbers = [32, 15, 3, 2, -5, 56, 10];
+
+const sumNumbers = numbers.reduce((result, number) => result + number); // O parâmetro `result` é o acumulador. Ele recebe, do `reduce`, o retorno da função a cada iteração.
+console.log(sumNumbers); // 113
+
+// Ou seja:
+
+const getSum = (result, number) => result + number;
+const sumNumbers = numbers.reduce(getSum);
+console.log(sumNumbers); // 113
+
+//
+
+//EXEMPLO : 
+
+const numbers = [32, 15, 3, 2, -5, 56, 10];
+
+const getSum = (result, number) => {
+  console.log(result); // 32 47 50 52 47 103
+  return result + number;
+};
+
+const sumNumbers = numbers.reduce(getSum);
+console.log(sumNumbers); // 113
+
+// 
+
+const numbers = [32, 15, 3, 2, -5, 56, 10];
+
+const getSum = (result, number) => {
+  console.log(result); // 0 32 47 50 52 47 103
+  return result + number;
+  };
+const sumNumbers = numbers.reduce(getSum, 0); // Parâmetro adicionado ao reduce: o "0"
+console.log(sumNumbers); // 113
+
+//
+
+const numbers = [32, 15, 3, 2, -5, 56, 10];
+
+const getSum = (result, number) => {
+  console.log(result); // 10 42 57 60 62 57 113
+  return result + number;
+  };
+const sumNumbers = numbers.reduce(getSum, 10);
+console.log(sumNumbers); // 123
+
+// <-------------------->
+
+const numbers = [50, 85, -30, 3, 15];
+
+const getBigger = (bigger, number) => ((bigger > number) ? bigger : number);
+
+const bigger = numbers.reduce(getBigger, 100);
+console.log(bigger); // 85
+
+
+
+
