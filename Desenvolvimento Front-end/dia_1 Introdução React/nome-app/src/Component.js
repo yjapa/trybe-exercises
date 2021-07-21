e9920e6d3583d57e5d5d322c0da0b563af1f77b9
Japa <wesleyy_@live.com>
2021-07-21 12:39:30 -0300

    Exercicios

---
+++ Desenvolvimento Front-end/dia_1 Introdução React/nome-app/src/Component.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+class description extends Component {
+  render () {
+    return <div>
+      <hi>Wesley Yudi</hi>
+      <p>Moro em SP, tenho 23 anos.</p>
+    </div>
+  }
+}
+
+export default description;
