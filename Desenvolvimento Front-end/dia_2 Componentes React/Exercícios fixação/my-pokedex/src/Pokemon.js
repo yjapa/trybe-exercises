d41f6f9433f8688df9f911982b47e5cb5ac8e546
Japa <wesleyy_@live.com>
2021-07-21 20:24:58 -0300

    Exercicios finalizados

--- Desenvolvimento Front-end/dia_2 Componentes React/Exercícios fixação/my-pokedex/src/Pokemon.js
+++ Desenvolvimento Front-end/dia_2 Componentes React/Exercícios fixação/my-pokedex/src/Pokemon.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
-class Pokemon extends React.Component {
+class Pokemon extends Component {
   render() {
     const { name, type, averageWeight, image } = this.props.poke;
 
@@ -22,14 +22,14 @@ class Pokemon extends React.Component {
 
 Pokemon.propTypes = {
   pokemon: PropTypes.shape({
-    name: PropTypes.string,
-    type: PropTypes.string,
+    name: PropTypes.string.isRequired,
+    type: PropTypes.string.isRequired,
     averageWeight: PropTypes.shape({
-      measurementUnit: PropTypes.string,
-      value: PropTypes.number,
+      measurementUnit: PropTypes.string.isRequired,
+      value: PropTypes.number.isRequired,
     }),
-    image: PropTypes.string,
-  }).isRequired,
+    image: PropTypes.string.isRequired,
+  })
 };
 
 export default Pokemon;
