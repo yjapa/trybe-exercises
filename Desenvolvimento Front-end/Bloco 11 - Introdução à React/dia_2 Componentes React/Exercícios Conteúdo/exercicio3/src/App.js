08ddb581aaf90e0b559f1571b1688a4e89a8697b
Japa <wesleyy_@live.com>
2021-07-28 14:05:06 -0300

    tentando

--- Desenvolvimento Front-end/Bloco 11 - Introdução à React/dia_2 Componentes React/Exercícios Conteúdo/exercicio3/src/App.js
+++ Desenvolvimento Front-end/Bloco 11 - Introdução à React/dia_2 Componentes React/Exercícios Conteúdo/exercicio3/src/App.js
@@ -8,7 +8,7 @@ class App extends React.Component {
   render() {
     const users = [
       {
-        id: 102,
+        id: 12,
         name: "João",
         email: "joao@gmail.com",
         avatar: "https:cdn.pixabay.com/photo/2016/08/20/05/38/avatar-1606916_640.png"
