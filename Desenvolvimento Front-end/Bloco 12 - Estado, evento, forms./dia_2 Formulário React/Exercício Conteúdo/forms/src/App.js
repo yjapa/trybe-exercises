671b0513e632c8340adffa9f4f7605c659591743
Japa <wesleyy_@live.com>
2021-07-28 16:51:18 -0300

    Exercicios conteudo realizado

--- Desenvolvimento Front-end/Bloco 12 - Estado, evento, forms./dia_2 Formulário React/Exercício Conteúdo/forms/src/App.js
+++ Desenvolvimento Front-end/Bloco 12 - Estado, evento, forms./dia_2 Formulário React/Exercício Conteúdo/forms/src/App.js
@@ -5,37 +5,83 @@ class App extends Component {
   constructor() {
     super();
 
-    this.handleLoginChange = this.handleLoginChange.bind(this);
+    this.handleChange = this.handleChange.bind(this);
 
     this.state = {
       login: '',
       password:'',
       textarea:'',
+      valor:'',
+      termos: false,
     };
   }
 
-  handleLoginChange(event) {
-    this.setState({login: event.target.value})
-  }
+handleChange({ target }) {
+  const { name } = target;
+  const value = target.type === 'checkbox' ? target.checked : target.value;
+
+  this.setState({
+    [name]: value
+  })
+}
 
   render() {
     return (
+      
       <div className="container">
         <form >
-          <label htmlFor=""> Login
-            <input 
-              name="login"
-              type="text"
-              value={this.state.login}
-              onChange={this.handleLoginChange}
-            />
-          </label>
-  
-          <label> Senha
-            <input name="password"type="password" />
-          </label>
+          <fieldset>
+          <legend>Informações pessoais</legend>
+            <label htmlFor=""> Login
+              <input 
+                name="login"
+                type="text"
+                value={this.state.login}
+                onChange={this.handleChange}
+              />
+            </label>
+    
+            <label> Senha
+              <input 
+                name="password"
+                type="password"
+                value={this.state.password}
+                onChange={this.handleChange}
+              />
+            </label>
+
+            <select
+              name="valor"
+              value={this.state.valor}
+              onChange={this.handleChange}
+            >
+              <option value="valor1">Valor 1</option>
+              <option value="valor2">Valor 2</option>
+              <option value="valor3">Valor 3</option>
+          </select>
+          </fieldset>
+
           <p>Digite algo:</p>
-          <textarea name="textarea" rows="4" cols="50"></textarea>
+
+          <textarea 
+            name="textarea"
+            rows="4" cols="50"
+            value={this.state.textarea}
+            onChange={this.handleChange}
+            ></textarea>
+
+            <div>
+              <label> Aceite os termos:
+                <input 
+                  name="termos"
+                  type="checkbox"
+                  value={this.state.termos}
+                  onChange={this.handleChange}
+                />
+              </label>
+            </div>
+
+            <input type="file"/>
         </form>
       </div>
     );
