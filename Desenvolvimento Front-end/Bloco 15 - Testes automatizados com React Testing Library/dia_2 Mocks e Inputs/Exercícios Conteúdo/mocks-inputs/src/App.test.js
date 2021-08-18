95dcf4dda5a34c50eb94634bd623cbd1dd623a19
Japa <wesleyy_@live.com>
2021-08-18 12:17:15 -0300

    att

--- Desenvolvimento Front-end/Bloco 15 - Testes automatizados com React Testing Library/dia_2 Mocks e Inputs/Exercícios Conteúdo/mocks-inputs/src/App.test.js
+++ Desenvolvimento Front-end/Bloco 15 - Testes automatizados com React Testing Library/dia_2 Mocks e Inputs/Exercícios Conteúdo/mocks-inputs/src/App.test.js
@@ -1,8 +1,56 @@
-import { render, screen } from '@testing-library/react';
+// App.test.js
+import React from 'react';
+import { render } from '@testing-library/react'
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+afterEach(() => jest.clearAllMocks());
+it('fetch joke', async () => {
+  const joke = {
+    id: '7h3oGtrOfxc',
+    joke: 'Whiteboards ... are remarkable.',
+    status: 200,
+  };
+
+  jest.spyOn(global, "fetch")
+  global.fetch.mockResolvedValue({
+    json: jest.fn().mockResolvedValue(joke),
+  });
+  const { findByText } = render(<App />);
+  await findByText('Whiteboards ... are remarkable.');
+  expect(global.fetch).toBeCalledTimes(1);
+  expect(global.fetch).toBeCalledWith(
+    'https://icanhazdadjoke.com/',
+    {"headers": {"Accept": "application/json"}}
+  );
 });
+
+// outro exemplo
+
+import React from 'react';
+import { render } from '@testing-library/react'
+import App from './App';
+
+afterEach(() => jest.clearAllMocks());
+it('should fetch users', async () => {
+  const joke = {
+    id: '7h3oGtrOfxc',
+    joke: 'Whiteboards ... are remarkable.',
+    status: 200,
+  };
+
+  global.fetch = jest.fn(()=>
+  Promise.resolve({
+    json: ()=> Promise.resolve(joke)
+  }));
+
+  const { findByText } = render(<App />);
+  await findByText('Whiteboards ... are remarkable.');
+  expect(global.fetch).toBeCalledTimes(1);
+  expect(global.fetch).toBeCalledWith('https://icanhazdadjoke.com/', {"headers": {"Accept": "application/json"}});
+});
+
+// outro exemplo
+
+global.fetch = jest.fn(async () => ({
+  json: async () => joke
+}));
