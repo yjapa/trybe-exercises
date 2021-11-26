a3d0f34f0347e1b4ac28966f78ab3b3cb40e741f
Japa <wesleyy_@live.com>
2021-11-26 15:58:43 -0300

    att

--- Fundamentos do Desenvolvimento Web/Bloco 4 - Introdução à JavaScript e Lógica de Programação/dia1_Primeiros Passos/scriptsV.js
+++ Fundamentos do Desenvolvimento Web/Bloco 4 - Introdução à JavaScript e Lógica de Programação/dia1_Primeiros Passos/scriptsV.js
@@ -17,3 +17,20 @@ switch (condicao) {
     console.log("Não se aplica");
 }
 
+function digits_sum(number_s) {
+  // Write your code here
+ let counter = 0;
+ for (let i = 0; i < 1000; i += 1) {
+     const splitNumbers = i.toString().split('')
+     const sumResult = splitNumbers.reduce((acc,act) => {
+         acc = parseInt(acc)  + parseInt(act)
+         return acc
+     }, 0)
+     if (sumResult === number_s) counter += 1
+ }
+return counter
+}
+
+console.log(digits_sum(26));
+
+console.log('digits_sum(26)');
